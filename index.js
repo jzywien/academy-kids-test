d1fe4560203c1f13237d0d36d206a5d373d74906
jzywien <jzywien@gmail.com>
2018-10-04 15:47:08 -0400

    add support for db page

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/db', (req, res) => res.render('pages/db', [{id: 1, name: 'test'}, {id: 2, name: 'test2'}]))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const results = {
+  results: [{id: 1, name: 'test'}, {id: 2, name: 'test2'}]
+};
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => res.render('pages/index'));
+app.get('/db', (req, res) => res.render('pages/db', results ));
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
